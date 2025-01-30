17b4daaa6122b013f2c32e36e812bf95b76adb59
Vidya2104 <vidyagangadhar2104@gmail.com>
2025-01-30 19:32:49 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -1,11 +1,11 @@
 // This is a simple Java program.
 // FileName : "f1.java".
 
-class f1 {
+class f2 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello lovely people");
+        System.out.println(" lovely people");
         System.out.println("I love you Vidya Ajay Aarna");
     }
 }
